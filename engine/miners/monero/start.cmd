9408049cd3ffbd64b70fb7daebd448459982b157
Gal Mrvar <glmrvar82@gmail.com>
2019-04-28 17:17:36 +0200

    Length done

--- engine/miners/monero/start.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-xmrig.exe --max-cpu-usage 65 -o xmr-eu1.nanopool.org:14433 -u 44znawrwDYiATG9MQy4qci5e2CWyFMLf5F22RVJAtJf9iS9heB3HMj3WbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3wVyYfD -p x -k
-pause
